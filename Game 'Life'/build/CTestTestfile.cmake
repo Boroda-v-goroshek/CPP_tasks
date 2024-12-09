c68c96038361fbd0ddc0ee03dbbf696b3dafd8ba
Boroda-V-Goroshek <Curobris@yandex.ru>
2024-12-09 16:08:23 +0700

    fix Game

--- Game 'Life'/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/babrakadabra/second_course/C/CPP/Game Life
-# Build directory: /home/babrakadabra/second_course/C/CPP/Game Life/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
--- Game 'Life'/build/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/babrakadabra/second_course/C/CPP/Game Life/lib
-# Build directory: /home/babrakadabra/second_course/C/CPP/Game Life/build/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
